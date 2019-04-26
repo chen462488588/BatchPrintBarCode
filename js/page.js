1a075289b280e58ad6d686966b958052b0f9f79b
chenkun <462488588@qq.com>
2019-04-26 18:09:26 +0800

    确认版本1

--- js/page.js
+++ js/page.js
@@ -1,48 +1,77 @@
 function generatePage() {
-    var startNum = $("#startCode").val();
+    var startNum = parseInt($("#startCode").val());
     var endNum = $("#endCode").val();
+    var preStr = $("#preStr").val();
 
-    var startBarCodeNum = "15C7-1" + startNum;
+
+    //要打印第几页
+    var pageNum = 0;
+    //总页数
+    var totalPage = parseInt((endNum - startNum) / 6) + 1;
     //存储条形码的编号
     var barCodeNums = new Array();
     //临时存储每个条形码的div包含的html
     var barCodeDIVs = new Array();
 
-    var temp = "";
-    for (var i = 1; i <= endNum - startNum + 1; i++) {
-        barCodeNums[i] = "15C7-1-" + i;
+    //每页包含的div
+    var pageBarCodeDiv = new Array(totalPage);
+
+    //默认值设置为"" ，防止初始化数组拼接后产生undefined
+    for (var i = 0; i < totalPage; i++) {
+        pageBarCodeDiv[i] = "";
+    }
+
+
+    for (var i = 0; i <= endNum - startNum; i++) {
+        barCodeNums[i] = preStr + (i + startNum);
         console.log(barCodeNums[i]);
-        if (i % 2 != 0) {
-            barCodeDIVs[i] = generateOneDIV("barCodeBox mr-10", "img" + i, barCodeNums[i]);
+
+        if (i % 2 != 0) {//条码若是奇数
+            barCodeDIVs[i] = generateBarCodeDiv("barCodeBox mr-10", "img" + (i + 1));
         } else {
-            barCodeDIVs[i] = generateOneDIV("barCodeBox", "img" + i, barCodeNums[i]);
+            barCodeDIVs[i] = generateBarCodeDiv("barCodeBox", "img" + (i + 1));
+        }
+        //每六个条码都要新增一页
+        if (i % 6 == 0) {
+            var pageDiv = generateOnePageDiv(++pageNum);
+            alert(pageDiv);
+            //把生成的页div填充到条码容器
+            $("#Container").append(pageDiv);
+
+
         }
 
-        temp = temp + barCodeDIVs[i];
+        //用数组来缓存每页的div信息
+        pageBarCodeDiv[pageNum - 1] = pageBarCodeDiv[pageNum - 1] + barCodeDIVs[i];
     }
-    //把拼接后的div一次性填充到条码容器div
-    $("#BarCodeContainer").html(temp);
+
+    for (var i = 0; i < totalPage; i++) {
+        //把每页拼接后的条形码div填充到条码容器div
+        $("#PageBarCodeContainer" + (i + 1)).html(pageBarCodeDiv[i]);
+    }
+
     //填充div后给每个div填充条形码
-    for (var i = 1; i <= endNum - startNum + 1; i++) {
-        JsBarcode(document.getElementById('img' + i), barCodeNums[i], options);
+    for (var i = 0; i <= endNum - startNum; i++) {
+        JsBarcode(document.getElementById('img' + (i + 1)), barCodeNums[i], options);
     }
 }
 
+//生成一个div用来装一页条形码
+function generateOnePageDiv(page) {
+    var pageDiv = "<div class=\"vertical-containers\" id=\"PageBarCodeContainer" + page + "\" style=\"page-break-after: always;\"" + " \">\n" +
+        "</div>";
+    return pageDiv;
+}
+
 //生成一个条形码的div
-function generateOneDIV(divClass, imgID) {
-    /*	var divContent = " <div class=\""+divClass+"\">\n" +
-            "        <div class=\"img\">\n" +
-            "            <img id=\""+imgID+"\"/>\n" +
-            "        </div>\n" +
-            "        <script>\n" +
-            "            var "+imgID+" = document.getElementById('"+imgID+"'),\n" +
-            "            JsBarcode("+imgID+","+barCode+", options);//原生\n" +
-            "        </script>\n" +
-            "    </div>";*/
-    var divContent = " <div class=\"" + divClass + "\">\n" +
+function generateBarCodeDiv(divClass, imgID) {
+
+    var barCodeDiv = " <div class=\"" + divClass + "\">\n" +
         "        <div class=\"img\">\n" +
         "            <img id=\"" + imgID + "\"/>\n" +
         "        </div>\n" +
         "    </div>";
-    return divContent;
+    return barCodeDiv;
 }
+
+
